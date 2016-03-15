61c965479f73a1fbca9f8cf79fff3e7d12383ff9
junyoung <junyoung@DESKTOP-46PRCDP>
2016-03-15 16:13:26 +0900

    최초 업로드

---
+++ HangeulBot/src/main/java/kr/co/hangeulbot/controller/HanguelBotController.java
@@ -0,0 +1,20 @@
+package kr.co.hangeulbot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
+public class HanguelBotController {
+	@RequestMapping("index.do")
+	public ModelAndView goIndex(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		return mav;
+	}
+	@RequestMapping("vowelPanixDIVver.do")
+	public ModelAndView goVowelPanixDIVver(){
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("vowelPanixDIVver");
+		return mav;
+	}
+}
