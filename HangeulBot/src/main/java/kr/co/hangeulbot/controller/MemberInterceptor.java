52af94b2bcda3504397e3295617074cef8128c13
junyoung <junyoung@DESKTOP-46PRCDP>
2016-03-25 18:54:44 +0900

    ㅇ

--- HangeulBot/src/main/java/kr/co/hangeulbot/controller/MemberInterceptor.java
+++ HangeulBot/src/main/java/kr/co/hangeulbot/controller/MemberInterceptor.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
+
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 public class MemberInterceptor extends HandlerInterceptorAdapter{
@@ -13,18 +14,26 @@ public class MemberInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request,HttpServletResponse response, Object handler) throws Exception {
 		Boolean flag = true;
-		HttpSession session =  request.getSession();
-		if(session!=null){
-			if(session.getAttribute("loginUserInfo")!=null){
-				flag = true;
+		String requestUri = request.getRequestURI().toString();
+		/*
+		if(!requestUrl.startsWith("http://192.168.0:8888/projectBlli4/")||request!=null){*/
+		if(requestUri.equals("/HangeulBot/goLoginPage.do")||requestUri.equals("/HangeulBot/memberLogin.do")){
+			
+		}else{
+			HttpSession session =  request.getSession();
+			if(session!=null){
+				if(session.getAttribute("loginUserInfo")!=null){
+					flag = true;
+				}else{
+					response.sendRedirect("goLoginPage.do");
+					flag = false;
+				}
 			}else{
 				response.sendRedirect("goLoginPage.do");
 				flag = false;
 			}
-		}else{
-			response.sendRedirect("goLoginPage.do");
-			flag = false;
 		}
+		
 		return flag;
 	}
 }
